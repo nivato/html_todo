916ac253d4e728d362a83feb6c2759de3dcdcb0d
nivato <nazarivato@gmail.com>
2014-05-30 12:55:54 +0300

    Implemented 'Remov'

--- todo.js
+++ todo.js
@@ -6,18 +6,29 @@ function add_text_row(){
 	text_input.val('');
 	var close_button = jQuery('<img/>', {src: 'close.png', class: 'close-button'});
 	close_button.click(delete_row);
+	close_button.hover(hover_close_button, unhover_close_button);
 	row.append(row_text, close_button);
 	$('.wrapper').append(row);
-};
+}
 
 function delete_row(event){
   var button = $(event.target);
   var row = button.parent();
   row.remove();
-};
+}
+
+function hover_close_button(event){
+	var button = $(event.target);
+	button.attr('src', 'close-hover.png');
+}
+
+function unhover_close_button(event){
+	var button = $(event.target);
+	button.attr('src', 'close.png');
+}
 
 function start(){
 	$('#add-button').click(add_text_row);
-};
+}
 
 $(document).ready(start);
